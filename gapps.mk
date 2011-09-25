941458cc722cd854ee0c1a9513a2e3bbce1d0557
Adam Green <greena88@gmail.com>
2011-09-25 15:21:12 +0100

    Initial commit (Gapps 2.3.7)

---
+++ gapps.mk
@@ -0,0 +1,25 @@
+# Google Apps
+PRODUCT_COPY_FILES += \
+    vendor/google/system/app/FOTAKill.apk:system/app/FOTAKill.apk \
+    vendor/google/system/app/GenieWidget.apk:system/app/GenieWidget.apk \
+    vendor/google/system/app/Gmail.apk:system/app/Gmail.apk \
+    vendor/google/system/app/GoogleBackupTransport.apk:system/app/GoogleBackupTransport.apk \
+    vendor/google/system/app/GoogleCalendarSyncAdapter.apk:system/app/GoogleCalendarSyncAdapter.apk \
+    vendor/google/system/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
+    vendor/google/system/app/GoogleFeedback.apk:system/app/GoogleFeedback.apk \
+    vendor/google/system/app/GooglePartnerSetup.apk:system/app/GooglePartnerSetup.apk \
+    vendor/google/system/app/GoogleQuickSearchBox.apk:system/app/GoogleQuickSearchBox.apk \
+    vendor/google/system/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
+    vendor/google/system/app/LatinImeTutorial.apk:system/app/LatinImeTutorial.apk \
+    vendor/google/system/app/MarketUpdater.apk:system/app/MarketUpdater.apk \
+    vendor/google/system/app/MediaUploader.apk:system/app/MediaUploader.apk \
+    vendor/google/system/app/NetworkLocation.apk:system/app/NetworkLocation.apk \
+    vendor/google/system/app/OneTimeInitializer.apk:system/app/OneTimeInitializer.apk \
+    vendor/google/system/app/SetupWizard.apk:system/app/SetupWizard.apk \
+    vendor/google/system/app/Talk2.apk:system/app/Talk2.apk \
+    vendor/google/system/app/Vending.apk:system/app/Vending.apk \
+    vendor/google/system/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
+    vendor/google/system/etc/permissions/features.xml:system/etc/permissions/features.xml \
+    vendor/google/system/framework/com.google.android.maps.jar:system/framework/com.google.android.maps.jar \
+    vendor/google/system/lib/libtalk_jni.so:system/lib/libtalk_jni.so
+
